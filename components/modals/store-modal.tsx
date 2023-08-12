4f97f27910e41d092e2bdd7da194bdbf56c85f86
WNagorny <walery.nagorny@gmail.com>
2023-08-12 15:58:26 +0200

    Prisma,PlanetScale,MySQL setup

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,6 +3,9 @@
 import * as z from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import {useState} from 'react'
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 import { useStoreModal } from '@/hooks/use-store-modal'
 import { Modal } from '@/components/ui/modal'
@@ -17,6 +20,8 @@ const formSchema = z.object({
 export const StoreModal = () => {
 	const storeModal = useStoreModal()
 
+	const [loading, setLoading] = useState(false)
+
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
@@ -25,8 +30,15 @@ export const StoreModal = () => {
 	})
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		console.log(values)
-		// TODO : create store
+		try {
+			setLoading(true);
+			const response = await axios.post('/api/stores', values);
+			toast.success("Store created")
+		 } catch (error) {
+			toast.error('Something went wrong');
+		 } finally {
+			setLoading(false);
+		 }
 	}
 
 	return (
@@ -47,7 +59,7 @@ export const StoreModal = () => {
 									<FormItem>
 										<FormLabel>Name</FormLabel>
 										<FormControl>
-										<Input placeholder="E-Commerce" {...field} />
+										<Input disabled={loading} placeholder="E-Commerce" {...field} />
 										</FormControl>
 										<FormMessage/>
 									</FormItem>
@@ -55,8 +67,8 @@ export const StoreModal = () => {
 							/>
 
 							<div className='pt-6 space-x-2 flex items-center justify-end w-full'>
-									<Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-									<Button type="submit">Continue</Button>
+									<Button disabled={loading} variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+									<Button disabled={loading} type="submit">Continue</Button>
 							</div>
 						</form>
 					</Form>
