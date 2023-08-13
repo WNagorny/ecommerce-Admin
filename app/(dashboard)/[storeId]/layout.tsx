4d2b9381a0bd466e96059bea42dc93d95c1cceaa
WNagorny <walery.nagorny@gmail.com>
2023-08-13 13:12:12 +0200

    created navbar structure

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs';
 
-// import Navbar from '@/components/navbar'
+import Navbar from '@/components/navbar'
 import prismadb from '@/lib/prismadb';
 
 export default async function DashboardLayout({
@@ -30,8 +30,8 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This will be a Navbar</div>  
+     <Navbar/>
       {children}
     </>
   );
-};
+}; 
