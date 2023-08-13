eb5f9722af9356da29acc290b0577c02e9a3615d
WNagorny <walery.nagorny@gmail.com>
2023-08-13 15:18:19 +0200

    created Settings Page part 2

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -4,6 +4,8 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb"
 
+import { SettingsForm } from "./components/settings-form";
+
 interface SettingsPageProps {
    params: {
       storeId:string;
@@ -33,9 +35,11 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({
     }
 
    return (
-      <div>
-         Hello Settings
+      <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
       </div>
+    </div>
    )
 }
 
