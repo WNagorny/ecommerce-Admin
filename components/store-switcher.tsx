8a915b465222cf94668294a0a884d0a20e92bca0
WNagorny <walery.nagorny@gmail.com>
2023-08-13 14:24:14 +0200

    added store switcher part 2

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -15,6 +15,16 @@ import {
 	PopoverTrigger,
 } from '@/components/ui/popover'
 
+import {
+   Command,
+   CommandEmpty,
+   CommandGroup,
+   CommandInput,
+   CommandItem,
+   CommandList,
+   CommandSeparator,
+ } from "@/components/ui/command"
+
 import { useStoreModal } from '@/hooks/use-store-modal'
 import { useParams, useRouter } from 'next/navigation'
 
@@ -60,14 +70,53 @@ export default function StoreSwitcher({
 					className={cn('w-[200px] justify-between', className)}
 				>
 					<StoreIcon className='mr-2 h-4 w-4' />
-               Current Store
-               
 					{currentStore?.label}
 					<ChevronsUpDown className='ml-auto h-4 w-4 shrink-0 opacity-50' />
 				</Button>
 			</PopoverTrigger>
 
-         
+         <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Search store..." />
+            <CommandEmpty>No store found.</CommandEmpty>
+            <CommandGroup heading="Stores">
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setOpen(false)
+                  storeModal.onOpen()
+                }}
+              >
+                <PlusCircle className="mr-2 h-5 w-5" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
 
 		</Popover>
 	)
