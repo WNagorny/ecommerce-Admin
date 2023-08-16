368cae777e4a1d3b652520435fce6d2805025ad0
WNagorny <walery.nagorny@gmail.com>
2023-08-16 18:14:32 +0200

    created column definitions

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,9 +6,17 @@ import { Separator } from "@/components/ui/separator";
 
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn } from './colums';
 
+interface BillboardClientProps {
+	data: BillboardColumn[];
+ }
 
-export const BillboardClient = () => {
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+
+	data
+
+}) => {
 
    const params = useParams();
    const router = useRouter();
@@ -17,7 +25,7 @@ export const BillboardClient = () => {
 		<>
 			<div className='flex items-center justify-between'>
 				<Heading
-					title='Billboards (0)'
+					title={`Billboards (${data.length})`}
 					description='Manage billboards for your store'
 				/>
 				<Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
