ba0f05d882f5586c735c88a022a3d2ab467630a3
WNagorny <walery.nagorny@gmail.com>
2023-08-17 14:35:00 +0200

    created sizes-form component

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -29,10 +29,15 @@ export function MainNav({
       label: 'Categories',
       active: pathname === `/${params.storeId}/categories`,
     },
+    {
+      href: `/${params.storeId}/sizes`,
+      label: 'Sizes',
+      active: pathname === `/${params.storeId}/sizes`,
+    },
    {
-     href: `/${params.storeId}/settings`,
-     label: 'Settings',
-     active: pathname === `/${params.storeId}/settings`,
+     href: `/${params.storeId}/sizes`,
+     label: 'Sizes',
+     active: pathname === `/${params.storeId}/sizes`,
    },
  ]
 
