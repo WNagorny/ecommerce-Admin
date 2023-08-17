ecfe0ad358070954cbfd16fdb83b045c03acd47e
WNagorny <walery.nagorny@gmail.com>
2023-08-17 11:56:58 +0200

    added update and delete functions to categories form

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -16,10 +16,10 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { AlertModal } from '@/components/modals/alert-modal'
 
-import { BillboardColumn } from './columns'
+import { CategoryColumn } from './columns'
 
 interface CellActionProps {
-	data: BillboardColumn
+	data: CategoryColumn
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -32,12 +32,12 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 	const onDelete = async () => {
 		try {
 			setLoading(true)
-			await axios.delete(`/api/${params.storeId}/billboards/${data.id}`)
-			toast.success('Billboard deleted.')
+			await axios.delete(`/api/${params.storeId}/categories/${data.id}`)
+			toast.success('Category deleted.')
 			router.refresh()
 		} catch (error) {
 			toast.error(
-				'Make sure you removed all categories using this billboard first.'
+				'Make sure you removed all products using this category first.'
 			)
 		} finally {
 			setOpen(false)
@@ -47,7 +47,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
 	const onCopy = (id: string) => {
 		navigator.clipboard.writeText(id)
-		toast.success('Billboard Id copied to clipboard.')
+		toast.success('Category Id copied to clipboard.')
 	}
 
 	return (
@@ -73,7 +73,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 					</DropdownMenuItem>
 					<DropdownMenuItem
 						onClick={() =>
-							router.push(`/${params.storeId}/billboards/${data.id}`)
+							router.push(`/${params.storeId}/categories/${data.id}`)
 						}
 					>
 						<Edit className='mr-2 h-4 w-4' /> Update
