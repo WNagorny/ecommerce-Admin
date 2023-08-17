5621c45791c5d5ac15f13ebbe97006f34e75260f
WNagorny <walery.nagorny@gmail.com>
2023-08-17 11:51:44 +0200

    added category-form and function adding new category

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -1,25 +1,31 @@
-import prismadb from '@/lib/prismadb'
+import prismadb from "@/lib/prismadb";
 
-import { BillboardForm } from "./components/billboard-form";
+import { CategoryForm } from "./components/category-form";
 
-const BillboardPage = async ({
-	params
+const CategoryPage = async ({
+  params
 }: {
-	params: { billboardId: string }
+  params: { categoryId: string, storeId: string }
 }) => {
-	const billboard = await prismadb.billboard.findUnique({
-		where: {
-			id: params.billboardId,
-		},
-	})
+  const category = await prismadb.category.findUnique({
+    where: {
+      id: params.categoryId
+    }
+  });
 
-	return (
-		<div className='flex-col'>
-			<div className='flex-1 space-y-4 p-8 pt-6'>
-         <BillboardForm initialData={billboard} />
-			</div>
-		</div>
-	)
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId
+    }
+  });
+
+  return ( 
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryForm billboards={billboards} initialData={category} />
+      </div>
+    </div>
+  );
 }
 
-export default BillboardPage
+export default CategoryPage;
