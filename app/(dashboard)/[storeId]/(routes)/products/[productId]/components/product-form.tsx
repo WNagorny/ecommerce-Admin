2e3be23940d913a5c6b11a0e9b7510ade381fa18
WNagorny <walery.nagorny@gmail.com>
2023-08-18 13:04:32 +0200

    added API routes and delete/update function

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -102,14 +102,14 @@ export const ProductForm: React.FC<ProductFormProps> = ({
 			setLoading(true)
 			if (initialData) {
 				await axios.patch(
-					`/api/${params.storeId}/billboards/${params.billboardId}`,
+					`/api/${params.storeId}/products/${params.productId}`,
 					data
 				)
 			} else {
-				await axios.post(`/api/${params.storeId}/billboards`, data)
+				await axios.post(`/api/${params.storeId}/products`, data)
 			}
 			router.refresh()
-			router.push(`/${params.storeId}/billboards`)
+			router.push(`/${params.storeId}/products`)
 			toast.success(toastMessage)
 		} catch (error: any) {
 			toast.error('Something went wrong.')
@@ -122,14 +122,14 @@ export const ProductForm: React.FC<ProductFormProps> = ({
 		try {
 			setLoading(true)
 			await axios.delete(
-				`/api/${params.storeId}/billboards/${params.billboardId}`
+				`/api/${params.storeId}/products/${params.productId}`
 			)
 			router.refresh()
-			router.push(`/${params.storeId}/billboards`)
-			toast.success('Billboard deleted.')
+			router.push(`/${params.storeId}/products`)
+			toast.success('Product deleted.')
 		} catch (error: any) {
 			toast.error(
-				'Make sure you removed all categories using this billboard first.'
+				'Something went wrong.'
 			)
 		} finally {
 			setLoading(false)
